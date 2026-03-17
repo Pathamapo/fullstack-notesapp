a351e04fe7cac1957d6feccfeb76c8d02f3e327e
Pathamapo <66160253@go.buu.ac.th>
2026-03-17 20:39:58 +0700

    Update Note.js

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -4,16 +4,21 @@ import PropTypes from 'prop-types';
 const Note = ({ bgColor, note }) => {
 	const history = useHistory();
 
+	const handleClick = () => {
+		history.push(`/${note._id}`);
+	};
+
 	return (
-		<div
-			className={`cursor-pointer w-full p-4 mb-4 ${bgColor} text-bg-black rounded-lg sm:w-1/4 sm:inline-block sm:mr-4 md:w-1/5`}
-			onClick={() => history.push(`/${note._id}`)}
+		<button
+			type="button"
+			className={`cursor-pointer w-full p-4 mb-4 ${bgColor} text-bg-black rounded-lg sm:w-1/4 sm:inline-block sm:mr-4 md:w-1/5 text-left`}
+			onClick={handleClick}
 		>
 			<h3 className="text-2xl font-semibold truncate">{note.title}</h3>
 			<p className="mt-3 h-6 truncate sm:h-20 sm:overflow-hidden sm:whitespace-normal">
 				{note.content}
 			</p>
-		</div>
+		</button>
 	);
 };
 
